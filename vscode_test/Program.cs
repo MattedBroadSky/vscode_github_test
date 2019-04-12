e1b5b634b8db674b21de7521d90b62d36a78a11c
BroadSky <broadsky@live.cn>
2019-04-13 02:06:46 +0800

    start

---
+++ vscode_test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+
+namespace vscode_test
+{
+    class Program
+    {
+        public static void Main() => Console.WriteLine("Hello, World!");
+    }
+}
